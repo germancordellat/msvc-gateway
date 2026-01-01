6a107c9be3cede39fe4e7b57b54b2891ed8bf746
germancordellat <germancordellat@gmail.com>
2026-01-01 18:50:33 +0100

    Implementar filtro global en Gateway con manejo de token y configuración de respuesta

--- src/main/java/com/prueba/msvc/gateway/filters/SampleGlobalFilter.java
+++ src/main/java/com/prueba/msvc/gateway/filters/SampleGlobalFilter.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -12,19 +13,29 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class SampleGlobalFilter implements GlobalFilter {
+public class SampleGlobalFilter implements GlobalFilter, Ordered {
 
     private final Logger logger = LoggerFactory.getLogger(SampleGlobalFilter.class);
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        logger.info(">>> Pre Filter: Request URI -> {}", exchange.getRequest().getURI());
-        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            logger.info(">>> Post Filter: Response Status Code -> {}", exchange.getResponse().getStatusCode());
+        
+        ServerWebExchange exchangeMutated = exchange.mutate().request(exchange.getRequest().mutate().header("token", "123456").build()).build();
+
+        return chain.filter(exchangeMutated).then(Mono.fromRunnable(() -> {
+            String token = exchangeMutated.getRequest().getHeaders().getFirst("token");
+            logger.info("token: {}", token);
+
+            exchange.getResponse().getHeaders().add("token", token != null ? token : "no-token");
             exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "blue").build());
             exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
         }));
         
+    }
+
+    @Override
+    public int getOrder() {
+        return 100;
     }
 
 }
