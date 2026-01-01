4996d377eb859211726d40d2569e2ea537b1305f
germancordellat <germancordellat@gmail.com>
2026-01-01 19:01:55 +0100

    Agregar registro y manejo de cookies en SampleCookieGatewayFilterFactory

--- src/main/java/com/prueba/msvc/gateway/filters/SampleCookieGatewayFilterFactory.java
+++ src/main/java/com/prueba/msvc/gateway/filters/SampleCookieGatewayFilterFactory.java
@@ -1,7 +1,12 @@
 package com.prueba.msvc.gateway.filters;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 import reactor.core.publisher.Mono;
@@ -9,14 +14,21 @@ import reactor.core.publisher.Mono;
 @Component
 public class SampleCookieGatewayFilterFactory extends AbstractGatewayFilterFactory<SampleCookieGatewayFilterFactory.ConfigurationCookie> {
 
+    private final Logger logger = LoggerFactory.getLogger(SampleCookieGatewayFilterFactory.class);
+
     public SampleCookieGatewayFilterFactory() {
         super(ConfigurationCookie.class);
     }
     @Override
     public GatewayFilter apply(ConfigurationCookie config) {
         return (exchange, chain) -> {
+            logger.info("Executing pre filter of SampleCookieGatewayFilterFactory: {} - {}", config.message);
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-
+                Optional.ofNullable(config.value)
+                    .ifPresent(cookie -> {
+                        exchange.getResponse().addCookie(ResponseCookie.from(config.name, cookie).build());
+                    });
+                logger.info("Executing post filter of SampleCookieGatewayFilterFactory: {}", config.message);
             }));
         };
     }
